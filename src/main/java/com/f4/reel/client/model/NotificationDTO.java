d9da46e6c4b0ff145f94a609f231afbc48af29a8
Your Name <user@example.com>
2025-06-23 14:07:17 +0700

    Update: 2025-06-23 14:07:17 liquibase init csv

--- src/main/java/com/f4/reel/client/model/NotificationDTO.java
+++ src/main/java/com/f4/reel/client/model/NotificationDTO.java
@@ -36,7 +36,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
   NotificationDTO.JSON_PROPERTY_IS_READ,
   NotificationDTO.JSON_PROPERTY_CREATED_AT
 })
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-06-23T12:21:05.800642270+07:00[Asia/Ho_Chi_Minh]", comments = "Generator version: 7.13.0")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-06-23T12:26:39.485368339+07:00[Asia/Ho_Chi_Minh]", comments = "Generator version: 7.13.0")
 public class NotificationDTO {
   public static final String JSON_PROPERTY_ID = "id";
   @javax.annotation.Nonnull
